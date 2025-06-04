e648c73863a54fffdf424709ea269c5081da3071
Ppduck62 <2132029842@qq.com>
2025-06-04 18:52:43 +0800

    Delete src directory

--- src/main/java/com/onlineaddressbooksystem/service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.onlineaddressbooksystem.service;
-
-import com.onlineaddressbooksystem.dto.AdminLoginDTO;
-import com.onlineaddressbooksystem.vo.AdminVo;
-
-public interface AdminService {
-    AdminVo login(AdminLoginDTO adminLoginDTO);
-}
--- src/main/java/com/onlineaddressbooksystem/service/MajorService.java
+++
@@ -1,9 +0,0 @@
-package com.onlineaddressbooksystem.service;
-
-import com.onlineaddressbooksystem.vo.MajorVo;
-
-import java.util.List;
-
-public interface MajorService {
-    List<MajorVo> list();
-}
